1f1724cbaf52578cbe35e7f0fc4d26cd4384e231
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-09-18 13:01:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function makeGrid(){
+  var btn = document.createElement("button");
+  btn.setAttribute("type", "button")
+  btn.setAttribute("class", "my-btn")
+  btn.setAttribute("id", "myBtn")
+  document.getElementById("gridContainer").appendChild(btn)
+}
