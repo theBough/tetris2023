a3fc8f122a3a53eaf6ca5a8290d56e7864369920
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-09-18 13:32:27 -0500

    Looped it 200

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-html {
-  height: 100%;
-  width: 100%;
-  background-color: #8D0801;
-  text-align: center;
-  justify-content: center;
-}
-.my-title{
-  font-size: 46px;
-  color: #001427;
-  text-align:center;
-  justify-content: center;
-  border: solid 10px;
-  border-color:#001427 ;
-}
-.my-btn{
-  background-color:#F4D58D ;
-  width:30px;
-  height: 30px;
-}
-.grid-container{
-  display: grid;
-  grid-template-columns: auto auto auto auto auto auto auto auto auto auto;
-  background-color: #BF0603;
-  width: 300px;
-  height: auto;
+function makeGrid(){
+  for(var i=0 ; i< 200 ; i++){
+    var btn = document.createElement("button");
+    btn.setAttribute("class", "my-btn")
+    btn.setAttribute("id", i)
+    document.getElementById("gridContainer").appendChild(btn)
+  }
 }
