a9e11f47caeba9d9a2af19464c82f2e645fef8a9
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-09-26 13:17:33 -0500

    Create variables.js
    Made a file that will hold all the variables.

---
+++ variables.js
@@ -0,0 +1,10 @@
+var teeArray = [5,14,15,16,"rgb(39,125,161)","tee"]
+var cubeArray = [5,6,15,16,"rgb(87,117,144)", "cube"]
+var stickArray = ["rgb(77,144,142"]
+var elArray = ["rgb(67,170,139)"]
+var reverseElArray = ["rgb(144,190,109)"]
+var esArray = ["rgb(249,199,79)"]
+var reverseEsArray = ["rgb(249,132,74)"]
+var activeTetromino = []
+var backColor = "rgb(244, 213, 141)"
+var myGravity;
