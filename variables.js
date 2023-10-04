36c686e4fab846f23e5f9a6688ec2cd35b7fe104
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-10-04 13:51:27 -0500

    all the arrays have been finished.

--- variables.js
+++ variables.js
@@ -1,10 +1,10 @@
 var teeArray = [5,14,15,16,"rgb(39,125,161)","tee",0]
-var cubeArray = [5,6,15,16,"rgb(87,117,144)", "cube"]
-var stickArray = ["rgb(77,144,142"]
-var elArray = ["rgb(67,170,139)"]
-var reverseElArray = ["rgb(144,190,109)"]
-var esArray = ["rgb(249,199,79)"]
-var reverseEsArray = ["rgb(249,132,74)"]
+var cubeArray = [5,6,15,16,"rgb(87,117,144)", "cube",0]
+var stickArray = [5,15,25,35"rgb(77,144,142","stick",0]
+var elArray = [5,15,25,26,"rgb(67,170,139)","el",0]
+var reverseElArray = [5,15,25,24,"rgb(144,190,109)","reverseEl",0]
+var esArray = [4,5,15,16,"rgb(249,199,79)","es",0]
+var reverseEsArray = [5,6,14,15,"rgb(249,132,74)","reverseEs",0]
 var activeTetromino = []
 var backColor = "rgb(244, 213, 141)"
 var myGravity;
