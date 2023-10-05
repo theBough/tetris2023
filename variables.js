1dd323f2806b7018b08fe84dcb4f7d1ba0754820
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-10-05 13:18:53 -0500

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,6 +1,6 @@
 var teeArray = [5,14,15,16,"rgb(39,125,161)","tee",0]
 var cubeArray = [5,6,15,16,"rgb(87,117,144)", "cube",0]
-var stickArray = [5,15,25,35"rgb(77,144,142","stick",0]
+var stickArray = [5,15,25,35,"rgb(77,144,142","stick",0]
 var elArray = [5,15,25,26,"rgb(67,170,139)","el",0]
 var reverseElArray = [5,15,25,24,"rgb(144,190,109)","reverseEl",0]
 var esArray = [4,5,15,16,"rgb(249,199,79)","es",0]
