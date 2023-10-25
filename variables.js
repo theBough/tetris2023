cfc5368a5c322711482384f29d234e11d2f5b2f1
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-10-25 12:33:52 -0500

    Update variables.js
    Had a typo with Es and reverseEs, and I added the nextTetromino

--- variables.js
+++ variables.js
@@ -3,10 +3,11 @@ var cubeArray = [5,6,15,16,"rgb(87,117,144)", "cube",0]
 var stickArray = [5,15,25,35,"rgb(77,144,142)","stick",0]
 var elArray = [5,15,25,26,"rgb(67,170,139)","el",0]
 var reverseElArray = [5,15,24,25,"rgb(144,190,109)","reverseEl",0]
-var esArray = [4,5,15,16,"rgb(249,199,79)","es",0]
-var reverseEsArray = [5,6,14,15,"rgb(249,132,74)","reverseEs",0]
+var reverseEsArray = [4,5,15,16,"rgb(249,199,79)","reverseEs",0]
+var esArray = [5,6,14,15,"rgb(249,132,74)","es",0]
 var activeTetromino = []
 var backColor = "rgb(244, 213, 141)"
 var myGravity;
 var finishedArray=[];
+var nextTetromino = [];
 
